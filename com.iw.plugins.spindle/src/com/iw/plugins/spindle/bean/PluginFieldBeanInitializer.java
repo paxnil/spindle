46291c1fdf01189811f2b7c18e3d64449f7a0ac3
Geoff Longman <glongman@gmail.com>
2002-06-16 16:20:08 +0000

    Final changes for F3 release

---
+++ com.iw.plugins.spindle/src/com/iw/plugins/spindle/bean/PluginFieldBeanInitializer.java
@@ -0,0 +1,20 @@
+package com.iw.plugins.spindle.bean;
+
+/**
+ * @author administrator
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public class PluginFieldBeanInitializer {
+
+  /**
+   * Constructor for PluginFieldBeanInitializer.
+   */
+  public PluginFieldBeanInitializer() {
+    super();
+  }
+
+}
---
+++ com.iw.plugins.spindle/src/com/iw/plugins/spindle/ui/FieldPropertyDescriptor.java
@@ -0,0 +1,20 @@
+package com.iw.plugins.spindle.ui;
+
+/**
+ * @author administrator
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public class FieldPropertyDescriptor {
+
+  /**
+   * Constructor for FieldPropertyDescriptor.
+   */
+  public FieldPropertyDescriptor() {
+    super();
+  }
+
+}
