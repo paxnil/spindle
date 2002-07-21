be88a3a0a85a141a08d3b34a1a8832efcac0cd4e
Geoff Longman <glongman@gmail.com>
2002-07-21 15:07:31 +0000

    *** empty log message ***

--- com.iw.plugins.spindle/src/com/iw/plugins/spindle/spec/PluginComponentSpecification.java
+++ com.iw.plugins.spindle/src/com/iw/plugins/spindle/spec/PluginComponentSpecification.java
@@ -241,22 +241,11 @@ public class PluginComponentSpecification
     propertySupport.firePropertyChange(event);
     StringWriter writer = new StringWriter();
     
-    System.out.println("PLUGIN_COMPONENT_SPEC_CHANGED!" + this);
-    write(new PrintWriter(writer));
-    System.out.println(writer.toString());
+ 
   }
   
-  private StringWriter poo;
-
   public void write(PrintWriter writer) {
-  	if (poo == null) {
-  	poo = new StringWriter();	
-    System.out.println("WRITING TO DISK!" + this);
-    write(new PrintWriter(poo));
-    System.out.println(poo.toString());
-    poo = null;
-    
-  	}
+  	
   		
   	String DTDversion = getDTDVersion();
   	boolean isDTD12 = DTDversion != null && "1.2".equals(DTDversion);
