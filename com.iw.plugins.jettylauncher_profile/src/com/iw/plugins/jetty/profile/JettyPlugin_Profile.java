c2641e7b6e110bbec1bf7245ecee426e5f26721b
Geoff Longman <glongman@gmail.com>
2003-06-13 11:44:55 +0000

    updated license and toc file

--- com.iw.plugins.jettylauncher_profile/src/com/iw/plugins/jetty/profile/JettyPlugin_Profile.java
+++ com.iw.plugins.jettylauncher_profile/src/com/iw/plugins/jetty/profile/JettyPlugin_Profile.java
@@ -11,7 +11,7 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * The Original Code is Jetty Launcher
+ * The Original Code is Jetty Launcher Profile
  *
  * The Initial Developer of the Original Code is
  * Intelligent Works Incorporated.
