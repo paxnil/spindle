5c36e4489279fff809bf65b2af6041d3ab0e7d2b
Geoff Longman <glongman@gmail.com>
2005-05-19 02:45:23 +0000

    make things compile in 3.1M7

--- com.iw.plugins.spindle.ui/src/com/iw/plugins/spindle/ui/wizards/project/BaseNewTapestryProjectJavaPage.java
+++ com.iw.plugins.spindle.ui/src/com/iw/plugins/spindle/ui/wizards/project/BaseNewTapestryProjectJavaPage.java
@@ -47,7 +47,6 @@ import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.internal.ui.util.ExceptionHandler;
 import org.eclipse.jdt.internal.ui.wizards.ClassPathDetector;
-import org.eclipse.jdt.internal.ui.wizards.NewWizardMessages;
 import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.jdt.ui.wizards.JavaCapabilityConfigurationPage;
 import org.eclipse.jface.operation.IRunnableContext;
