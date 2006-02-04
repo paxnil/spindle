5be0edc1a159f6b177b9359ed0a11af3e7664ca4
Geoff Longman <glongman@gmail.com>
2006-02-04 21:32:14 +0000

    moved to correct package, tweak impl and added a test

---
+++ spindle-libraries/spindle-core/src/test/java/net/sf/spindle/core/resources/JarClasspathResource.java
@@ -0,0 +1,158 @@
+package net.sf.spindle.core.resources;
+
+/*
+ The contents of this file are subject to the Mozilla Public License
+ Version 1.1 (the "License"); you may not use this file except in
+ compliance with the License. You may obtain a copy of the License at
+ http://www.mozilla.org/MPL/
+
+ Software distributed under the License is distributed on an "AS IS"
+ basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ License for the specific language governing rights and limitations
+ under the License.
+
+ The Original Code is __Spindle, an Eclipse Plugin For Tapestry__.
+
+ The Initial Developer of the Original Code is _____Geoffrey Longman__.
+ Portions created by _____Initial Developer___ are Copyright (C) _2004, 2005, 2006__
+ __Geoffrey Longman____. All Rights Reserved.
+
+ Contributor(s): __glongman@gmail.com___.
+ */
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Locale;
+
+import net.sf.spindle.core.TapestryCoreException;
+import net.sf.spindle.core.resources.ICoreResource;
+import net.sf.spindle.core.resources.IResourceAcceptor;
+import net.sf.spindle.core.resources.search.ISearch;
+
+import org.apache.hivemind.Resource;
+import org.apache.hivemind.util.AbstractResource;
+
+/**
+ * an implementation of {@link net.sf.spindle.core.resources.ICoreResource} for
+ */
+public class JarClasspathResource extends AbstractResource implements ICoreResource
+{
+
+    private JarClasspathRoot root;
+
+    /* package */JarClasspathResource(JarClasspathRoot root, String path)
+    {
+        super(path);
+        this.root = root;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.apache.hivemind.util.AbstractResource#newResource(java.lang.String)
+     */
+    @Override
+    protected Resource newResource(String path)
+    {
+        return root.newResource(path);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see net.sf.spindle.core.resources.ICoreResource#isClasspathResource()
+     */
+    public boolean isClasspathResource()
+    {
+        return root.isClasspathResource(this);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see net.sf.spindle.core.resources.ICoreResource#isBinaryResource()
+     */
+    public boolean isBinaryResource()
+    {
+        return root.isBinaryResource(this);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see net.sf.spindle.core.resources.ICoreResource#getContents()
+     */
+    public InputStream getContents()
+    {
+        return root.getContents(this);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see net.sf.spindle.core.resources.ICoreResource#clashesWith(net.sf.spindle.core.resources.ICoreResource)
+     */
+    public boolean clashesWith(ICoreResource resource)
+    {
+        return root.clashCkeck(this, resource);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see net.sf.spindle.core.resources.ICoreResource#isFolder()
+     */
+    public boolean isFolder()
+    {
+        return root.isFolder(this);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.apache.hivemind.Resource#getResourceURL()
+     */
+    public URL getResourceURL()
+    {
+        return root.getResourceURL(this);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.apache.hivemind.Resource#getLocalization(java.util.Locale)
+     */
+    public Resource getLocalization(Locale locale)
+    {
+        return root.getLocalization(this, locale);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see net.sf.spindle.core.resources.ResourceExtension#exists()
+     */
+    public boolean exists()
+    {
+        return root.exists(this);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see net.sf.spindle.core.resources.ResourceExtension#lookup(net.sf.spindle.core.resources.IResourceAcceptor)
+     */
+    public void lookup(IResourceAcceptor requestor)
+    {
+        root.lookup(this, requestor);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see net.sf.spindle.core.resources.ResourceExtension#getSearch()
+     */
+    public ISearch getSearch() throws TapestryCoreException
+    {
+        return root.getSearch();
+    }
+}
---
+++ spindle-libraries/spindle-core/src/test/java/net/sf/spindle/core/resources/JarClasspathRootTest.java
@@ -0,0 +1,92 @@
+package net.sf.spindle.core.resources;
+
+/*
+ The contents of this file are subject to the Mozilla Public License
+ Version 1.1 (the "License"); you may not use this file except in
+ compliance with the License. You may obtain a copy of the License at
+ http://www.mozilla.org/MPL/
+
+ Software distributed under the License is distributed on an "AS IS"
+ basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ License for the specific language governing rights and limitations
+ under the License.
+
+ The Original Code is __Spindle, an Eclipse Plugin For Tapestry__.
+
+ The Initial Developer of the Original Code is _____Geoffrey Longman__.
+ Portions created by _____Initial Developer___ are Copyright (C) _2004, 2005, 2006__
+ __Geoffrey Longman____. All Rights Reserved.
+
+ Contributor(s): __glongman@gmail.com___.
+ */
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+import net.sf.spindle.core.util.Files;
+
+import org.apache.hivemind.Resource;
+
+public class JarClasspathRootTest extends TestCase
+{
+
+    JarClasspathRoot root;
+
+    @Override
+    protected void setUp() throws Exception
+    {
+        root = getTapestryTestRoot();
+    }
+
+    private JarClasspathRoot getTapestryTestRoot() throws IOException, URISyntaxException
+    {
+        return new JarClasspathRoot("tapestryTest.jar");
+    }
+
+    public void test() throws IOException
+    {
+        Resource framework = root.getRelativeResource("org/apache/tapestry/Framework.library");
+
+        URL url = framework.getResourceURL();
+
+        assertNotNull(url);
+
+        InputStream input = url.openStream();
+
+        String all = Files.readFileToString(input, null);
+
+        // TODO remove
+        System.out.println(all);
+    }
+
+    public void testLookup()
+    {
+        ICoreResource framework = (ICoreResource) root
+                .getRelativeResource("org/apache/tapestry/Framework.library");
+        assertTrue(framework.exists());
+
+        IResourceAcceptor acceptor = new IResourceAcceptor()
+        {
+
+            private ArrayList<ICoreResource> results = new ArrayList<ICoreResource>();
+
+            public boolean accept(ICoreResource location)
+            {
+                results.add(location);
+                return true;
+            }
+
+            public ICoreResource[] getResults()
+            {
+                return results.toArray(new ICoreResource[] {});
+            }
+        };
+        framework.lookup(acceptor);
+
+        assertEquals(5, acceptor.getResults().length);
+    }
+
+}
